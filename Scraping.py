6be3e05d97a75a1f7b3ba56ee489e52a9d873f7e
KOBeerose <tahaelghabi@gmail.com>
2022-01-19 19:31:43 +0100

    baseline working

---
+++ Scraping.py
@@ -0,0 +1,16 @@
+import mechanize
+from bs4 import BeautifulSoup
+import urllib.request
+import http.cookiejar
+
+cj = http.cookiejar.CookieJar()
+br = mechanize.Browser()
+br.set_cookiejar(cj)
+br.open("https://www.topannonces.fr/")
+
+br.select_form(nr=0)
+br.form['username'] = 'username'
+br.form['password'] = 'password.'
+br.submit()
+
+print(br.response().read())
